102c0a861c0bc09d7466f383db9a0ebbbecdaf39
Jaime Bunzli <jpbunzli@gmail.com>
2016-05-20 11:48:55 -0400

    chore(): killer whale

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,82 +1,36 @@
 import {Component} from '@angular/core';
-import { Hero } from './hero';
+// import {REST} from './killer_whale';
 
-import { HeroDetailComponent } from './hero-detail.component';
+import {District} from './models/district.model';
 
-var HEROES: Hero[] = [
-  { id: 11, name: "Mr. Nice" },
-  { id: 12, name: "Narco" },
-];
+function REST(target: any, key: string) {
+  console.log(this);
+  debugger;
+  target[key] = [{
+        id: 12,
+        name: "Hola"
+    },
+    {
+        id: 13,
+        name: "Chao"
+    }];
+};
 
 @Component({
     selector: 'my-app',
-    directives: [HeroDetailComponent],
     template:`
-      <h2>My Heroes</h2>
+      <h2>Comunas</h2>
       <ul class="heroes">
-        <li *ngFor="let hero of heroes"
-          [class.selected]="hero === selectedHero"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
+        <li *ngFor="let district of districts">
+          {{district}}
         </li>
       </ul>
-      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    `,
-        styles:[`
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        background-color: #BBD8DC !important;
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #DDD;
-        left: .1em;
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0 0 4px;
-      }
-    `]
-
+    `
 })
 export class AppComponent {
-    heroes = HEROES;
+    @REST
+    districts: string[] = ["hola", "chao"];
     title = 'Tour of Heroes';
-    selectedHero: Hero;
-
-    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+
---
+++ app/killer_whale.ts
@@ -0,0 +1,39 @@
+function REST() {
+    return function(target: any, key: string) {
+        target = [{
+            id: 12,
+            name: "Hola"
+        },
+        {
+            id: 13,
+            name: "Chao"
+        }];
+    };
+}
+//   // property value
+//   var _val = this[key];
+ 
+//   // property getter
+//   var getter = function () {
+//     console.log(`Get: ${key} => ${_val}`);
+//     return _val;
+//   };
+ 
+//   // property setter
+//   var setter = function (newVal) {
+//     console.log(`Set: ${key} => ${newVal}`);
+//     _val = newVal;
+//   };
+ 
+//   // Delete property.
+//   if (delete this[key]) {
+ 
+//     // Create new property with getter and setter
+//     Object.defineProperty(target, key, {
+//       get: getter,
+//       set: setter,
+//       enumerable: true,
+//       configurable: true
+//     });
+//   }
+//}
